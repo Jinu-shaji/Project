6c22b2af17e7a545d91cbe12565f844a1a4c8b95
JINU <20mp1604@rit.ac.in>
2021-12-27 10:48:43 +0530

    design,login,home

---
+++ adminhome.php
@@ -0,0 +1,40 @@
+<html>
+<head>
+	<title>Div</title>
+<link rel="stylesheet" type="text/css" href="maincss.css">
+</head>
+<body >
+	<header>
+		<div class="main">
+	<ul>
+<li><a href="adminhome.php">Home</a></li>
+<li><a>Advisors</a>
+<ul>
+<li><a href="addadvisor.php">Add advisors</a></li>
+<li><a href="#">view advisors</a></li>
+
+</ul>
+</li>
+<li><a>#</a>
+<ul>
+<li><a href="#">D</a></li>
+<li><a href="#">E</a></li>
+</ul>
+</li>
+<li><a>#</a>
+<ul>
+<li><a  href="#">F</a></li>
+<li><a href="#">G</a></li>
+</ul>
+</li>
+</ul></div>
+		<div class="title">
+			<h3>PROJECT PIVOT </h3></div></header><br><br>
+<link rel="stylesheet" type="text/css" href="form.css"><br><br><br><br><br>
+<center><h1><b><font face="Bahnschrift Condensed" >WELCOME ADMIN</font></b></h1></center>
+
+
+
+
+
+</body>
